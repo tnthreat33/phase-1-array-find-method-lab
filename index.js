b7798ea87f93aeddd990c93c87d188d2ef8da7d2
Rachel Hamby <tnthreat33@gmail.com>
2023-09-30 10:29:32 -0400

    commit tree f70e503088ea534e3f44167062d6286c94e4e8d0

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(games){
+    let result = games.find(game => game.result === "W")
+       if (result){
+        return result.year
+       } else{
+        return undefined
+       }
+    
+   
+    
+}
